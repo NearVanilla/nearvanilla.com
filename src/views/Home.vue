b1ff2b3f8733637501fc762f96175418b2993b36
Alistair Spragg <alistair.spragg@gmail.com>
2021-08-01 18:37:59 +0100

    Test tailwind & mq match

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,27 @@
 <template>
-  <div></div>
+  <div
+    class="
+      w-screen
+      h-screen
+      bg-blue-100
+      sm:bg-blue-200
+      md:bg-blue-300
+      lg:bg-blue-400
+      xl:bg-blue-500
+      flex
+      justify-center
+      items-center
+    "
+  >
+    <p class="text-5xl font-bold">{{ mq.current }}</p>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Home",
+
+  inject: ["mq"],
 };
 </script>
 
