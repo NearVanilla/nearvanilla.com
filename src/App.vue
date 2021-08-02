078149b520dddc3b8efb8bcddf621f15dbc62c80
Alistair Spragg <alistair.spragg@gmail.com>
2021-08-02 17:05:45 +0100

    Add default styles

--- src/App.vue
+++ src/App.vue
@@ -21,5 +21,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  width: 100vw;
+  
+}
+
+body{
+  margin:0;
+  padding:0;
+  overflow-x: hidden;
+  -webkit-tap-highlight-color: #fed136;
+}
+
+.montserrat{
+  font-family: Montserrat, 'Helvetica Neue', Helvetica, Arial, sans-serif;
 }
 </style>
