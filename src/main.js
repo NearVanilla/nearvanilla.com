a83dcaf7e51504d76e3d492e4ce5aaf1f62f7f62
Alistair Spragg <alistair.spragg@gmail.com>
2021-08-02 17:06:25 +0100

    Add smooth scroll and fa icons

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 
 import "./main.css";
+import './reset.css';
 
 import router from "./router";
 import { store } from "./store";
@@ -10,13 +11,15 @@ import { Vue3Mq } from "vue3-mq";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
+import VueSmoothScroll from 'vue3-smooth-scroll'
+
 /* Font Awesome icons */
-import { faBars } from "@fortawesome/free-solid-svg-icons";
-library.add(faBars);
+import { faBars, faCaretDown } from "@fortawesome/free-solid-svg-icons";
+library.add(faBars, faCaretDown);
 
 var app = createApp(App);
 app.use(router).use(store).use(Vue3Mq, {
   preset: "tailwind",
-});
+}).use(VueSmoothScroll);
 app.component("font-awesome-icon", FontAwesomeIcon);
 app.mount("#app");
