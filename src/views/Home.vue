2a6fab68de1faf6969378c5c2428b95141431c98
Alistair Spragg <alistair.spragg@gmail.com>
2021-08-02 17:06:35 +0100

    Implement new nav

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,58 @@
 <template>
-  <div
-    class="
-      w-screen
-      h-screen
-      bg-blue-100
-      sm:bg-blue-200
-      md:bg-blue-300
-      lg:bg-blue-400
-      xl:bg-blue-500
-      flex
-      justify-center
-      items-center
-      flex-col
-    "
-  >
-    <p class="text-5xl font-bold">{{ mq.current }}</p>
-    <p><font-awesome-icon :icon="['fas', 'bars']" /></p>
+  <div class="w-screen scroll-container" id="page-top">
+    <navbar :shrunk="shrunkNav" />
+    <div>
+      <div class="w-full h-screen bg-img" :style="{backgroundImage: 'url(' + bg + ')' }"></div>
+      <div class="h-screen">
+        <p class="p-0 text-5xl font-bold">{{ mq.current }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { Navbar } from "../components";
+import { ref } from "vue";
+import bgImg from '../assets/img/bg-1.jpg'
+
 export default {
   name: "Home",
 
+  components: {
+    Navbar,
+  },
+
   inject: ["mq"],
+
+  setup(props, context) {
+    const shrunkNav = ref(false);
+    const bg = bgImg;
+    
+    const handleScroll = (e) => {
+      if (window.scrollY > 100) {
+        shrunkNav.value = true;
+      } else shrunkNav.value = false;
+    };
+
+    return { shrunkNav, bg, handleScroll };
+  },
+
+  created() {
+    window.addEventListener("scroll", this.handleScroll);
+  },
+
+  mounted() {},
+
+  destroyed() {
+    window.removeEventListener("scroll", () => {});
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.bg-img {
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: center center;
+}
+</style>
