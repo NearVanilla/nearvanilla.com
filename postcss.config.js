accd197b5ca1a30ce8531372aefdb441253049e7
Alistair Spragg <alistair.spragg@gmail.com>
2021-08-02 17:04:32 +0100

    Improve postcss purging

--- postcss.config.js
+++ postcss.config.js
@@ -1,22 +1,17 @@
-const tailwindcss = require("tailwindcss");
-const autoprefixer = require("autoprefixer");
-const purgecss = require("@fullhuman/postcss-purgecss");
+const purgecss = require('@fullhuman/postcss-purgecss')
+const cssnano = require('cssnano')
 
 module.exports = {
-    plugins: [
-        tailwindcss("./tailwind.config.js"),
-
-        autoprefixer({
-            add: true,
-            grid: true
-        }),
-
-        //Only add purgecss in production
-        process.env.NODE_ENV === "production" ? purgecss({
-            content: [
-                "./src/**/*.html",
-                "./src/**/*.vue"
-            ]
-        }) : ""
-    ]
-};
+  plugins: [
+    require('tailwindcss'),
+    process.env.NODE_ENV === 'production' ? require('autoprefixer') : null,
+    process.env.NODE_ENV === 'production'
+      ? cssnano({ preset: 'default' })
+      : null,
+    process.env.NODE_ENV === 'production' ? purgecss({
+      content: ['./layouts/**/*.html', './src/**/*.vue', './src/**/*.jsx'],
+      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+      safelist: ['py-1.5']
+    }) : null
+  ]
+}
