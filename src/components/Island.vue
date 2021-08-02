b800ea2058d84bd14b1bbf824ed6da19a278e826
Alistair Spragg <alistair.spragg@gmail.com>
2021-08-02 21:19:54 +0100

    Add floating island graphic

---
+++ src/components/Island.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="relative">
+    <img
+      src="../assets/img/leftIsland.png"
+      alt="logoLeftIsland"
+      class="relative pos-left"
+    />
+    <img
+      src="../assets/img/rightIsland.png"
+      alt="logoRightIsland"
+      class="rightisland rightislandfloat"
+    />
+    <img
+      src="../assets/img/middleIslandsWithClouds.png"
+      alt="logoMiddleIsland"
+      class="centerisland centerislandfloat"
+    />
+    <img
+      src="../assets/img/NVText2.png"
+      alt="logoText"
+      class="logofont logofontglow"
+    />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
