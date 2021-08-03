1a8d1ff2ac456a00589d618b655742f5ffb3cb4d
Alistair Spragg <alistair.spragg@gmail.com>
2021-08-03 23:13:16 +0100

    Add axios

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 
 import "./main.css";
-import './reset.css';
+import "./reset.css";
+import "./glider.min.css";
 
 import router from "./router";
 import { store } from "./store";
@@ -11,15 +12,24 @@ import { Vue3Mq } from "vue3-mq";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-import VueSmoothScroll from 'vue3-smooth-scroll'
+import VueSmoothScroll from "vue3-smooth-scroll";
 
 /* Font Awesome icons */
 import { faBars, faCaretDown } from "@fortawesome/free-solid-svg-icons";
 library.add(faBars, faCaretDown);
 
+import axios from "axios";
+
 var app = createApp(App);
-app.use(router).use(store).use(Vue3Mq, {
-  preset: "tailwind",
-}).use(VueSmoothScroll);
+app
+  .use(router)
+  .use(store)
+  .use(Vue3Mq, {
+    preset: "tailwind",
+  })
+  .use(VueSmoothScroll);
+
+app.config.globalProperties.$http = axios;
+
 app.component("font-awesome-icon", FontAwesomeIcon);
 app.mount("#app");
