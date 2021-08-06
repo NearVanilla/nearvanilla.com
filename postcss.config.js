bf77554f8c2c7846095f3e41e449a1b1596b22cd
Alistair Spragg <alistair.spragg@gmail.com>
2021-08-06 09:22:29 +0100

    Add carousel classes to whitelist

--- postcss.config.js
+++ postcss.config.js
@@ -5,13 +5,15 @@ module.exports = {
   plugins: [
     require('tailwindcss'),
     process.env.NODE_ENV === 'production' ? require('autoprefixer') : null,
+
     process.env.NODE_ENV === 'production'
       ? cssnano({ preset: 'default' })
       : null,
+
     process.env.NODE_ENV === 'production' ? purgecss({
       content: ['./layouts/**/*.html', './src/**/*.vue', './src/**/*.jsx'],
       defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-      safelist: ['py-1.5']
+      whitelist: ['py-1.5, carousel, carousel__viewport, carousel__track']
     }) : null
   ]
 }
