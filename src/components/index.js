866701c40a639e007d3b761345ce631b6658d521
Alistair Spragg <alistair.spragg@gmail.com>
2021-08-19 10:44:42 +0100

    Add plugin components

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Navbar from "./Navbar.vue";
 import Island from "./Island.vue";
 import Members from "./Members.vue";
 import Map from "./Map.vue";
+import Plugins from "./Plugins.vue";
 
-export { Map, Navbar, Island, Members };
+export { Map, Navbar, Island, Members, Plugins };
