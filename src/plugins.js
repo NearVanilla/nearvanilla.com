ee790f53593a85ca7b397a65c220aa5616a09941
Alistair Spragg <alistair.spragg@gmail.com>
2021-08-19 10:44:09 +0100

    Added type

--- src/plugins.js
+++ src/plugins.js
@@ -4,6 +4,7 @@ export const pluginsList = [
     excerpt: "Always know where exactly you died. No F3 screen required!",
     info: "Say Goodbye to watching ugly F3 screen in case a creeper sneaks up on you. Your death coordinates will be sent to you in a private message each time you die.",
     on: "spigot",
+    type: "custom",
   },
   {
     name: "Dynmap",
@@ -11,12 +12,14 @@ export const pluginsList = [
       "A Google Maps-like map for our Minecraft server that can be viewed in a browser.",
     info: "Dynmap allows us to create beautiful maps of our server, so that you always know where you going.",
     on: "spigot",
+    type: "core",
   },
   {
     name: "CapAFK",
     excerpt: "A simple yet core plugin that allows us to kick AFK players.",
     info: "Once the player has been playing for 3 hours, the player will have 60 seconds to type in a capatcha. If the player can't do that, then the player gets kicked from the server.",
     on: "",
+    type: "custom",
   },
   {
     name: "CoreProtect",
@@ -24,6 +27,7 @@ export const pluginsList = [
       "CoreProtect is a fast, efficient, data logging and anti-griefing tool.",
     info: "Rollback and restore any amount of damage. Designed with large servers in mind, CoreProtect will record and manage data without impacting our server's performance. Thanks to it, we don't need claim plugins and are able to seamlessly protect our players.",
     on: "spigot",
+    type: "administration",
   },
   {
     name: "DiscordSRV",
@@ -31,24 +35,28 @@ export const pluginsList = [
       "Powerful, configurable, open-source Discord to Minecraft bridging plugin.",
     info: "Using this plugin, we are able to give players the ability to chat in-game to chat with players on our Discord server as well as having people on the Discord server be able to chat with people on the server",
     on: "spigot",
+    type: "core",
   },
   {
     name: "One Player Sleep",
     excerpt: "Have the bed just for yourself. We don't judge!",
     info: "Sometimes people AFK for a moment, or maybe they are away from bed. This plugin allows for skipping the night, as long as at least one player is in bed and noone wants to keep the night.",
     on: "spigot",
+    type: "custom",
   },
   {
     name: "IsSlimeChunk",
     excerpt: "A simple plugin show chunk is Slime chunk or not.",
     info: "This plugin will display in chat if the chunk you're standing on is a slime chunk or not. This way you don't need the seed. <br/> Use the following command: <br /> /slime?",
     on: "spigot",
+    type: "core",
   },
   {
     name: "LuckPerms",
     excerpt: "Dialing just right level of permission for all of our players.",
     info: "We can easily manage all of our players and give everyone all the permissions they need.",
     on: "spigot",
+    type: "administration",
   },
   {
     name: "worldborder",
@@ -56,5 +64,6 @@ export const pluginsList = [
       "This is basic card with image on top, title, description and button.",
     info: "This plugin is intended to efficiently provide a border for each of your worlds, which only people granted special bypass access are allowed beyond. These passses can be bought in our  <br/> <a href='https://nearvanilla.buycraft.net/'>donation shop</a>",
     on: "bukkit",
+    type: "core",
   },
 ];
