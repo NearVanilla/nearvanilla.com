44158d7d31f755e51f92afd3af6422862a96de2e
Alistair Spragg <alistair.spragg@gmail.com>
2022-05-09 13:53:51 +0100

    Fix url

--- src/components/DownloadCard.vue
+++ src/components/DownloadCard.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="w-full bg-gray-900 rounded-lg border border-gray-200 shadow-md">
-    <a href="https://files.nearvanilla.com/worlds/NearVanillaS1.zip">
+    <a :href="downloadUrl">
       <img class="rounded-t-lg" :src="imgUrl" alt="" />
     </a>
     <div class="p-2 text-left tracking-tight">
-      <a href="https://files.nearvanilla.com/worlds/NearVanillaS1.zip">
+      <a :href="downloadUrl">
         <h5 class="mb-2 font-bold text-gray-100">Season {{ season }}</h5>
       </a>
       <p class="mb-2 font-normal text-gray-300">
@@ -28,4 +28,5 @@ import { computed } from "vue";
 
 const props = defineProps(["season", "size", "date", "version"]);
 const imgUrl = computed(() => new URL(`/img/spawn/s${props.season}.png`, import.meta.url));
+const downloadUrl = computed(() => new URL(`https://files.nearvanilla.com/worlds/NearVanillaS${props.season}.zip`, import.meta.url));
 </script>
